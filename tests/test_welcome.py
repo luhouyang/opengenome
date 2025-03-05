ebf0562b1ed0d0fa4b9c344b3de0e434675059d1
Lu Hou Yang <luhouyang@gmail.com>
2025-03-05 12:20:36 +0800

    test: added type safety to test_about

--- tests/test_welcome.py
+++ tests/test_welcome.py
@@ -4,9 +4,9 @@ from opengenome.welcome import about
 
 
 @pytest.fixture
-def welcome_val():
+def welcome_val() -> int:
     return 1
 
 
-def test_welcome(welcome_val):
+def test_welcome(welcome_val: int) -> None:
     assert welcome_val == about(), "Welcome did not return 1"
