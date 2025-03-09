d6b40cf68cf134ec39dc746eb21e3fd40c118f06
luhouyang <luhouyang@gmail.com>
2025-03-10 02:07:32 +0800

    build: added data fils and website information

--- setup.py
+++ setup.py
@@ -1,19 +1,28 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_namespace_packages, find_packages
 
 with open('README.md', 'r') as f:
     description = f.read()
 
 setup(
     name='opengenome',
-    version='0.1.3',
+    version='0.1.4',
     long_description=description,
     long_description_content_type="text/markdown",
-    packages=find_namespace_packages(where="src"),
+    packages=find_namespace_packages(
+        where="src",
+        include=['opengenome', 'opengenome.*'],
+        exclude=[],
+    ),
+    package_data={"": ["*.txt"]},
     license_files=('LICENSE.txt', ),
     package_dir={'': 'src'},
-    install_requires=[],
+    python_requires=">=3.10",
+    install_requires=[
+        "torch>=2",
+        "matplotlib>=3",
+        "torchinfo>=1.8",
+    ],
     include_package_data=True,
-    package_data={"": ["*.yaml"]},
     entry_points={
         "console_scripts": [
             "opengenome-about = opengenome:about",
@@ -30,5 +39,9 @@ setup(
             "sphinx-autoapi",
             "pydata_sphinx_theme",
         ]
+    },
+    url="https://opengenome.readthedocs.io/en/latest/",
+    project_urls={
+        "Source": "https://github.com/luhouyang/opengenome",
     },
 )
