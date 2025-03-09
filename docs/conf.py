fa2b7b44a6acfc63848b462f2e76f1040355667c
luhouyang <luhouyang@gmail.com>
2025-03-09 21:48:39 +0800

    docs: updated theme and logos for rtd website

--- docs/conf.py
+++ docs/conf.py
@@ -34,3 +34,23 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # a list of builtin themes.
 #
 html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
+html_logo = "_static/logo.png"
+html_css_files = ["css/custom.css"]
+html_theme_options = {
+    "logo": {
+        "link": "https://open-genome-project.org"
+    },
+    "icon_links": [{
+        "name": "GitHub",
+        "url": "https://github.com/luhouyang/opengenome.git",
+        "icon": "fab fa-github",
+        "type": "fontawesome",
+    }],
+    "external_links": [
+        {
+            "name": "website",
+            "url": "https://open-genome-project.org"
+        },
+    ]
+}
