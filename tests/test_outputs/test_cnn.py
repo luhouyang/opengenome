9e3ed4d174435c3a2995de0987072c341ed587dc
Lu Hou Yang <luhouyang@gmail.com>
2025-03-12 11:59:15 +0800

    test: updated output.cnn test

--- tests/test_outputs/test_cnn.py
+++ tests/test_outputs/test_cnn.py
@@ -2,10 +2,12 @@ from opengenome.outputs.cnn import viz
 import torch
 
 
-def test_viz():
+def test_viz() -> None:
     from torchvision.models import vgg16, VGG16_Weights
     DEVICE = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    with open('tests/data/VGG16_CLASSES.txt', 'r') as f:
+        classes = f.readlines()
     model = vgg16(weights=VGG16_Weights.DEFAULT)
     model = model.to(DEVICE)
     img_car = "tests/data/car_01.png"
-    viz(10, img_car, model, DEVICE)
+    viz(10, img_car, model, classes, DEVICE)
